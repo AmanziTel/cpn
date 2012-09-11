6dd78b25d47a7783d514823f7d926fba48e6b02b
Craig Taverner <craig@amanzi.com>
2012-09-12 01:14:19 +0200

    Refactored generic build commands out of dsl_builder

---
+++ lib/cpn/builder.rb
@@ -0,0 +1,19 @@
+require File.expand_path("#{File.dirname __FILE__}/dsl_builder")
+require File.expand_path("#{File.dirname __FILE__}/json_builder")
+
+module CPN
+
+  # This method will build a time-based network, which might be
+  # composed of sub-networks, or pages.
+  def self.build(name, path = nil, &block)
+    DSLBuilder.build_net(name, path, &block)
+  end
+
+  # This method also builds a time based network, but based on
+  # a JSON specification instead of a Ruby DSL specification.
+  def self.build_json(name, json)
+    JSONBuilder.build_net(name, json)
+  end
+
+end
+
